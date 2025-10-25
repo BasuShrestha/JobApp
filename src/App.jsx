c6143fd8e076cc10fc75eb7830486b9a201bcb39
Basu Shrestha <basu.stha4@gmail.com>
2025-10-25 12:19:47 +0545

    Added layouts and created routes

--- src/App.jsx
+++ src/App.jsx
@@ -5,10 +5,17 @@ import {
   RouterProvider,
 } from "react-router-dom";
 
+import MainLayout from "./layouts/MainLayout";
 import HomePage from "./pages/HomePage";
+import JobsPage from "./pages/JobsPage";
 
 const router = createBrowserRouter(
-  createRoutesFromElements(<Route index element={<HomePage />} />)
+  createRoutesFromElements(
+    <Route path="/" element={<MainLayout />}>
+      <Route index element={<HomePage />} />
+      <Route path="/jobs" element={<JobsPage />} />
+    </Route>
+  )
 );
 
 const App = () => {
--- src/pages/HomePage.jsx
+++ src/pages/HomePage.jsx
@@ -1,5 +1,8 @@
 import React from "react";
 import Hero from "../components/Hero";
+import HomeCard from "../components/HomeCard";
+import JobListings from "../components/JobListings";
+import ViewAllJobsBtn from "../components/ViewAllJobsBtn";
 
 const HomePage = () => {
   return (
@@ -8,6 +11,9 @@ const HomePage = () => {
         title="Become a React Developer"
         subtitle="Find the React job that fits your skills and needs"
       />
+      <HomeCard />
+      <JobListings />
+      <ViewAllJobsBtn />
     </>
   );
 };
